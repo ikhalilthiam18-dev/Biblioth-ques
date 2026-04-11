9de4b47491cfa5e9b2b6367519f7216d3c28a7fb
ikhalilthiam18-dev <i.khalil.thiam18@gmail.com>
2026-04-11 23:37:18 +0000

    final fix

--- bibliotheque_project/settings.py
+++ bibliotheque_project/settings.py
@@ -7,7 +7,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-dev-key-change-in-production')
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    'localhost',
+    'biblioth-ques.vercel.app',
+    '.vercel.app',
+]
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
