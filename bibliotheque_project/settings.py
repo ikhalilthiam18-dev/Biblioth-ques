06dd3ae456bf915d89cb0e73b0988fc912af8c43
ikhalilthiam18-dev <i.khalil.thiam18@gmail.com>
2026-04-16 12:40:28 +0000

    'requierments'

--- bibliotheque_project/settings.py
+++ bibliotheque_project/settings.py
@@ -59,15 +59,17 @@ WSGI_APPLICATION = 'bibliotheque_project.wsgi.application'
 
 
 DATABASE_URL = os.environ.get("DATABASE_URL")
-
-DATABASES = {
-    "default": dj_database_url.parse(DATABASE_URL, conn_max_age=600)
-} if DATABASE_URL else {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+if os.environ.get("DATABASE_URL"):
+    DATABASES = {
+        'default': dj_database_url.config(conn_max_age=600)
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
